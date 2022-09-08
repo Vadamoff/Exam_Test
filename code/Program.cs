7deb16f65b1bccd9fb60a6374b7159eb441acaaa
Vadamoff <Vadamoff@mail.ru>
2022-09-08 22:53:48 +0300

    Added CreateTextArray & PrintTextArray

--- code/Program.cs
+++ code/Program.cs
@@ -9,3 +9,32 @@
 лучше обойтись исключительно массивами.
 */
 
+string[] CreateTextArray()
+{
+    Console.WriteLine("Задайте количество элементов в массиве:");
+    int size = int.Parse(Console.ReadLine());
+    string input = String.Empty;
+    string[] textArr = new string[size];
+    int i = 0;
+    do
+    {
+        Console.WriteLine($"Введите строку {i + 1}:");
+        input = Console.ReadLine();
+//        Console.WriteLine(input);
+        textArr[i] = input;
+        i++;
+    }
+    while (i < size);
+    return textArr;
+}
+
+void PrintTextArray(string[] textArr)
+{
+    for (int i = 0; i < textArr.Length; i++)
+        Console.WriteLine(textArr[i]);
+}
+string[] textArray = CreateTextArray();
+
+Console.WriteLine("Вы создали следующий массив:");
+
+PrintTextArray(textArray);
