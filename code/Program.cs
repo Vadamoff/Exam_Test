a6e61503aa8706ba37129b03202b4475b25addc0
Vadamoff <Vadamoff@mail.ru>
2022-09-09 11:28:31 +0300

    Finished code

--- code/Program.cs
+++ code/Program.cs
@@ -9,6 +9,8 @@
 лучше обойтись исключительно массивами.
 */
 
+//Метод создаёт массив.
+//Количество элементов и сами элементы задаются пользователем.
 string[] CreateTextArray()
 {
     Console.WriteLine("Задайте количество элементов в массиве:");
@@ -20,6 +22,7 @@ string[] CreateTextArray()
     {
         Console.WriteLine($"Введите строку {i + 1}:");
         input = Console.ReadLine();
+//        Console.WriteLine("Вы ввели:");
 //        Console.WriteLine(input);
         textArr[i] = input;
         i++;
@@ -28,13 +31,56 @@ string[] CreateTextArray()
     return textArr;
 }
 
+//Метод выводит массив.
 void PrintTextArray(string[] textArr)
 {
     for (int i = 0; i < textArr.Length; i++)
-        Console.WriteLine(textArr[i]);
+        Console.Write($"{textArr[i]}    ");
 }
+
+//Метод подсчитывает количество элементов массива,
+//длина которых не превышает 3 символов.
+int CalcLengthArray(string[] textArr)
+{
+    int sum = 0;
+    for(int i = 0; i < textArr.Length; i++)
+    {
+        if(textArr[i].ToCharArray().Length <= 3)
+            sum++;
+    }
+//    Console.WriteLine("Количество элементов массива, длина которых не превышает 3 символов:");
+//    Console.WriteLine(sum);
+    return sum;
+}
+
+//Метод добавляет в новый массив
+//все элементы входящего массива,
+//длина которых не превышает 3 символов.
+string[] RemakeArray(string[] textArr, int size)
+{
+    string[] newTextArr = new string[size];
+    int j = 0;
+    for(int i = 0; i < textArr.Length; i++)
+    {
+        if(textArr[i].ToCharArray().Length <= 3)
+        {
+            newTextArr[j] = textArr[i];
+            j++;
+        }
+    }
+    return newTextArr;
+}
+
 string[] textArray = CreateTextArray();
 
-Console.WriteLine("Вы создали следующий массив:");
+//Console.WriteLine("Вы создали следующий массив:");
 
-PrintTextArray(textArray);
+//PrintTextArray(textArray);
+
+int arraySize = CalcLengthArray(textArray);
+
+string[] newTextArray = RemakeArray(textArray, arraySize);
+
+Console.WriteLine("Новый массив из строк, длина элементов которого не превышает 3 символов:");
+
+PrintTextArray(newTextArray);
